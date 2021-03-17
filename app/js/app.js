9ed7b7e347ec7f2767484e17ca05e852345f7ed1
alebul <master8423@gmail.com>
2021-03-17 10:29:15 +0300

    sum values of object

--- app/js/app.js
+++ app/js/app.js
@@ -4,11 +4,28 @@
 // window.$ = $
 // // Import vendor jQuery plugin example (not module)
 // require('~/app/libs/mmenu/dist/mmenu.js')
-import debug from "./lib";
+import debug, {isEmpty} from "./lib";
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    const user = {name: "Alex"};
-    user.name = "Mixa";
-    debug(user);
+
+    let salaries = {
+        John: 100,
+        Ann: 160,
+        Pete: 130
+    }
+
+    let empty = {}
+
+    let sum = (obj) => {
+        let sum = 0;
+        for (let key in obj) {
+            sum += obj[key];
+        }
+        return sum;
+    }
+
+    debug(sum(salaries));
+    debug(sum(empty));
+
 })
--- app/js/lib.js
+++ app/js/lib.js
@@ -2,4 +2,11 @@ function debug(obj) {
     console.log(obj);
 }
 
+export function isEmpty(obj) {
+    for (let key in obj) {
+        return true;
+    }
+    return false;
+}
+
 export default debug;
